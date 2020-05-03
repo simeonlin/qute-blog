122a8ee06e494116003c5729be38d755748036ac
simeon.liniger <simeon.liniger@hftm.ch>
2020-05-03 14:58:32 +0200

    Add Search-String to index

--- src/main/java/ch/hftm/vsblog/web/IndexController.java
+++ src/main/java/ch/hftm/vsblog/web/IndexController.java
@@ -1,12 +1,14 @@
 package ch.hftm.vsblog.web;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.SecurityContext;
@@ -30,10 +32,19 @@ public class IndexController {
 
     @GET
     @Path("{a: |index.html}")
-    public TemplateInstance getIndex() {
+    public TemplateInstance getIndex(@QueryParam("searchString") String searchString) {
         LocalDateTime actDate = LocalDateTime.now();
         System.out.println("INDEX loaded: " +actDate);
-        return index.data("entries", Entry.listAll(Sort.by("id", Direction.Descending))).data("lastreload", actDate);
+        List<Entry> entries;
+
+        if(searchString == null || searchString.isEmpty()) {
+            entries = Entry.listAll(Sort.by("id", Direction.Descending));
+        } else {
+            entries = Entry.list("title LIKE ?1 OR content LIKE ?1", Sort.by("id", Direction.Descending),"%"+searchString+"%");
+        }
+        
+
+        return index.data("entries", entries).data("lastreload", actDate);
     }
 
     @GET
